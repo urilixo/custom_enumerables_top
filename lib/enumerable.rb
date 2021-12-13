5d49eeb343cfeb06b5b2de949c2da8e403d326cb
urilixo <falecomelvio@hotmail.com>
2021-12-13 17:34:46 -0300

    added my_each_with_index

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -4,8 +4,41 @@ module Enumerable
       yield i
     end
   end
+
+  def my_each_with_index
+    for i in 0..self.length do
+      yield self[i], i
+    end
+  end
+
+  def my_select
+  end
+
+  def my_all?
+  end
+
+  def my_any?
+  end
+
+  def my_none?
+  end
+
+  def my_count
+  end
+  
+  def my_map
+  end
+
+  def my_inject
+  end
 end
 
-numbers = [1, 2, 3, 4, 5]
-numbers.my_each { |item| puts item }
-numbers.each { |item| puts item }
+numbers = [5, 5, 2, 6, 5]
+#numbers.my_each { |item| puts item }
+#numbers.each { |item| puts item }
+
+numbers.my_each_with_index{ |item, index| puts "#{item}: index#{index}"}
+numbers.each_with_index { |item, index| puts "#{item}: index#{index}"}
+
+
+
