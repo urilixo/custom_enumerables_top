4a728659bf2e82d3c4bf3c2c16e021317a321471
urilixo <falecomelvio@hotmail.com>
2021-12-13 17:26:10 -0300

    my_each complete

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,4 +1,11 @@
 module Enumerable
   def my_each
-  end    
+    for i in self do
+      yield i
+    end
+  end
 end
+
+numbers = [1, 2, 3, 4, 5]
+numbers.my_each { |item| puts item }
+numbers.each { |item| puts item }
